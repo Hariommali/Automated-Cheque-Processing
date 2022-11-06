6758b01f50fcaaa2ccac0f522e5a2ce3db4b5e52
Hari Om Mali <65865291+Hariompc1@users.noreply.github.com>
2022-11-06 13:41:37 +0530

    Add files via upload

---
+++ Prototype/remove_file.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+
+int main()
+{
+system("rm lal1.jpg ");
+system("rm lal2.jpg ");
+system("rm lal3.jpg ");
+system("rm lal4.jpg ");
+system("rm lal5.jpg ");
+
+system("rm lft_fin.BMP ");
+system("rm lft_fin.txt ");
+system("rm rgt_fin.BMP ");
+system("rm rgt_fin.txt ");
+
+system("rm omr.jpg");
+system("rm shrilal1.jpg");
+system("rm shrilal2.jpg");
+system("rm shrilal3.jpg");
+system("rm shrilal4.jpg");
+system("rm shrilal5.jpg");
+
+system("rm shrilalmali1.jpg");
+system("rm shrilalmali2.jpg");
+system("rm shrilalmali3.jpg");
+system("rm shrilalmali4.jpg");
+system("rm shrilalmali5.jpg");
+
+system("rm tenlakh.txt");
+system("rm tenthous.txt");
+system("rm thous.txt");
+system("rm lakh.txt");
+system("rm crore.txt");
+
+system("rm un_prfy_omr.jpg");
+
+
+return 0;
+}
