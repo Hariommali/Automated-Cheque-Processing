906f6711ee051789da3c982f6dfa9786ece28858
Hari Om Mali <65865291+Hariompc1@users.noreply.github.com>
2022-11-15 15:03:53 +0530

    Add files via upload

---
+++ Prototype/software.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+os.system("clear")
+os.system ("apt upgrade -y")
+os.system ("apt update -y")
+os.system ("pkg install opencv-python -y")
+os.system ("apt update && apt upgrade -y")
+os.system ("pip install --upgrade pip")
+os.system ("pip install pillow")
+os.system ("pip install imutils")
